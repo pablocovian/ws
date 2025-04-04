8cf5a41ab4ac0514c19073970bcd59fba18eac4b
pablocovian <pablosabatev2@gmail.com>
2025-04-03 18:33:04 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+/* script.js */
+document.addEventListener("DOMContentLoaded", () => {
+    const text = document.querySelector(".animated-text");
+    text.style.animation = "fadeInUp 2s ease-out forwards";
+});

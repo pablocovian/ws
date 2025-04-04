5e27d89d6318e84981b0b94d98fb3342c0171cf3
pablocovian <pablosabatev2@gmail.com>
2025-04-03 18:45:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* script.js */
 document.addEventListener("DOMContentLoaded", () => {
-    const text = document.querySelector(".animated-text");
-    text.style.animation = "fadeInUp 2s ease-out forwards";
+    console.log("Orbit animation is running!");
 });
